e251a38fa11ef726a9b3b867e5fc1299652fc48e
Paras dhiman <parasdhiman@192.168.1.12>
2026-07-01 13:37:31 +0530

    fixed a bug

--- core/asgi.py
+++ core/asgi.py
@@ -1,15 +1,16 @@
 import os
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from chat.consumers import ChatConsumer, HomeConsumer
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter([
             path("ws/chat/<int:partner_id>/", ChatConsumer.as_asgi()),
